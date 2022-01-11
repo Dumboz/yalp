2ea934e96782577d58ce0de95fb189d89dc6ff6d
kheeyaa <kheeyaa@icloud.com>
2022-01-11 17:48:35 +0900

    fix: Modify color pick logic
    1. import getHexaColor with changed logic
    2. Modify getHexaColor parameter
    3. Set proptypes

--- src/components/Star/Star.jsx
+++ src/components/Star/Star.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { getHexaColor } from 'styles/color';
 
-/*
-    state: 'empty', 'full', 'half'
-    color: 'gray', 'yellow', 'red', 'orange'
-*/
-const starImg = (state = 'empty', color = 'GRAY') => {
+const starImg = (state = 'empty', step = 500) => {
   return (
     <svg
       width="23"
@@ -18,11 +15,19 @@ const starImg = (state = 'empty', color = 'GRAY') => {
         width="23"
         height="24"
         rx="3"
-        fill={state === 'half' ? getHexaColor('gray') : getHexaColor(color)}
+        fill={
+          state === 'half' || state === 'empty'
+            ? getHexaColor('gray', 200)
+            : getHexaColor('primary', step)
+        }
       />
       <path
         d="M0 3C0 1.34315 1.34315 0 3 0H12V24H3C1.34315 24 0 22.6569 0 21V3Z"
-        fill={state === 'empty' ? getHexaColor('gray') : getHexaColor(color)}
+        fill={
+          state === 'empty'
+            ? getHexaColor('gray', 200)
+            : getHexaColor('primary', step)
+        }
       />
       <path
         d="M12 4L14.5863 8.44033L19.6085 9.52786L16.1846 13.3597L16.7023 18.4721L12 16.4L7.29772 18.4721L7.81535 13.3597L4.39155 9.52786L9.41374 8.44033L12 4Z"
@@ -32,8 +37,13 @@ const starImg = (state = 'empty', color = 'GRAY') => {
   );
 };
 
-function Star({ state, color }) {
-  return starImg(state, color);
+function Star({ state, step }) {
+  return starImg(state, step);
 }
+
+Star.propTypes = {
+  state: PropTypes.string,
+  color: PropTypes.number || PropTypes.string,
+};
 
 export default Star;
