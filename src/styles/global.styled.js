213622e606a33f3e895232eecbd1e0a3b55038e2
kheeyaa <kheeyaa@icloud.com>
2022-01-12 21:23:04 +0900

    config: Update global styled js file
    1. Install styled reset package for reset css
    2. Add reset css code in GlobalStyle component

--- src/styles/global.styled.js
+++ src/styles/global.styled.js
@@ -1,8 +1,33 @@
 import { createGlobalStyle } from 'styled-components/macro';
+import reset from 'styled-reset';
 
 export const GlobalStyle = createGlobalStyle`
-  :root {
-    --color: #267ac9;
+  ${reset}
+    *{
+        box-sizing:border-box;
+    }
+    body{
+        font-size:14px;
+        font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+    }
+    a{
+        text-decoration:none;
+        color:inherit;
+        cursor: pointer;
+    }
+    ol, ul, li {
+        list-style: none;
+    }
+    img {
+        display: block;
+        width: 100%;
+        height: 100%;
+    }
+    input, button {
+        background-color: transparent;
+    }
+    h1, h2, h3, h4, h5, h6 {
+    font-family:'Maven Pro', sans-serif;
   }
 
   body {
