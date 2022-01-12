8244622484418e8dae3e7ad22740a485ace05b88
윤석규 <yunseoggyu@yunseoggyuui-MacBookAir.local>
2022-01-12 13:46:11 +0900

    feat: Add Accessibility option to Selection #5

--- src/components/Selection/Selection.jsx
+++ src/components/Selection/Selection.jsx
@@ -4,13 +4,24 @@ export function Selection({ key, type, name, children, checked }) {
   const id = name + key;
   return (
     <List key={key}>
-      <Input type={type} name={name} id={id} checked={checked}></Input>
+      <Input
+        type={type}
+        name={name}
+        id={id}
+        role="switch"
+        aria-checked={checked}
+        checked={checked}
+      />
       <StyledLabel
         type={type}
         htmlFor={id}
         checked={checked}
-        className={checked ? 'active' : 'deactive'}></StyledLabel>
-      <span>{children}</span>
+        className={checked ? 'active' : 'deactive'}
+        aria-labelledby={'contents'}
+      />
+      <span id="contents" tabIndex="0">
+        {children}
+      </span>
     </List>
   );
 }
