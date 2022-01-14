488f1298a22715bf4eb7c40a9f3c71851f6aa865
SeokGyu Yoon <49303508+dbstjrrb12@users.noreply.github.com>
2022-01-14 14:37:22 +0900

    Revert "Fix Tags, Arrow button and Price Filter Button "

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,20 +4,38 @@ import { InitSVG, Icon, Comment } from 'components';
 import { CommentList } from 'components/CommentList/CommentList';
 import { PriceFilterButton } from 'components/PriceFilterButton/PriceFilterButton';
 import { UserInfo } from 'components/UserInfo/UserInfo';
-import { Tags } from 'components/Tags/Tags';
-import { ArrowButton } from 'components/ArrowButton/ArrowButton';
-import { PriceFilterButtonGroup } from 'components/PriceFilterButton/PriceFilterButtonGroup';
 
 export function App() {
   return (
     <>
       <div className='App'>Yalp!</div>
+      {/* <PriceFilterButton />
+      <DetailMenuCard /> */}
+      <UserInfo />
+
+      {/* <Icon type="calendar" size={100} /> */}
+      {/* <Comment title="안녕" iconType="calendar" size={32}>
+        I dream about this place. And I'm a very picky pizza eater. Super saucy,
+        fresh, thick, perfectly greasy, hot, yum, yum. Plus the employees are
+        totally great and looking out for the hihihihihihihihihihihihi
+      </Comment> */}
+
+      <CommentList>
+        <Comment title='안녕' iconType='calendar' size={32}>
+          I'm calendar
+        </Comment>
+        <Comment title='불' iconType='fire' size={32}>
+          I'm fire
+        </Comment>
+        <Comment title='저장' iconType='save' size={32}>
+          I'm save
+        </Comment>
+      </CommentList>
+
       {/* 한 번이상 렌더링 되지 않는 로직 작성하기 */}
-      <PriceFilterButton isSelect={false}>111111</PriceFilterButton>
-      <PriceFilterButtonGroup />
       <InitSVG />
-      <ArrowButton size={24} type='leftarrow' isDisabled={true} />
-      <Tags size={16}>안녕하세요</Tags>
+      <PriceFilterButton />
+      <DetailMenuCard />
     </>
   );
 }
