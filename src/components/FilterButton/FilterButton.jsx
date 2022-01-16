4338faaf311af62e28c17864a4a6e5f85ae378bb
윤석규 <yunseoggyu@yunseoggyuui-MacBookAir.local>
2022-01-16 21:12:23 +0900

    feat: Add FilterButton folder

---
+++ src/components/FilterButton/FilterButton.jsx
@@ -0,0 +1,3 @@
+export const FilterButton = () => {
+  return <div></div>;
+};
---
+++ src/components/FilterButton/FilterButton.stories.jsx
@@ -0,0 +1,11 @@
+import { array } from 'prop-types';
+import { FilterButton } from './FilterButton';
+
+export default {
+  title: 'FilterButton',
+  component: FilterButton,
+};
+
+const Template = (args) => <FilterButton {...args} />;
+
+export const Button = Template.bind({});
