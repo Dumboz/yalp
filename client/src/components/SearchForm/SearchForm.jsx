d4396c7ef9669fbb8370b210f5f4aa2bacffb371
kheeyaa <kheeyaa@icloud.com>
2022-01-19 14:00:47 +0900

    feat: Update SearchForm (Add onSubmit function)
    1. Add useNavigate to route url path
    2. Modify deacription -> term because of Yelp API query string

--- client/src/components/SearchForm/SearchForm.jsx
+++ client/src/components/SearchForm/SearchForm.jsx
@@ -3,10 +3,13 @@ import Button from 'components/Button/Button';
 import React, { useState, useCallback } from 'react';
 import { getAutocomplete } from 'api';
 import { Label, Text, Input, Form } from './SearchForm.styled';
+import { useSearchParams, useNavigate } from 'react-router-dom';
+import { makeQuery } from 'utils';
 
 function SearchForm({ showLabel, hasShadow, searchWord, locationWord }) {
   const [location, setLocation] = useState(locationWord);
   const [autoTerms, setAutoTerms] = useState();
+  const navigate = useNavigate();
 
   const onChange = useCallback(
     e => {
@@ -18,10 +21,13 @@ function SearchForm({ showLabel, hasShadow, searchWord, locationWord }) {
   const onSubmit = useCallback(e => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    console.log('onsubmit');
+    const formObj = {};
+
     for (const [key, value] of formData.entries()) {
-      console.log({ key, value });
+      formObj[key] = value;
     }
+    navigate('/businesses/search?' + makeQuery(formObj));
+    e.preventDefault();
   }, []);
 
   const onAutocomplete = useCallback(
@@ -43,13 +49,13 @@ function SearchForm({ showLabel, hasShadow, searchWord, locationWord }) {
         {showLabel ? <Text>Find</Text> : <A11yHidden>Find</A11yHidden>}
         <Input
           onChange={onAutocomplete}
-          name="description"
-          id="description"
-          list="descriptionList"
+          name="term"
+          id="term"
+          list="termList"
           placeholder={searchWord}
         />
       </Label>
-      <datalist autoComplete="off" id="descriptionList">
+      <datalist autoComplete="off" id="termList">
         {autoTerms && autoTerms.map(term => <option value={term} key={term} />)}
       </datalist>
       <Label width={464}>
