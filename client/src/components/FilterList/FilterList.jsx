ff521adeea048715b2616b4c662b88ef25a8f5d6
윤석규 <yunseoggyu@yunseoggyuui-MacBookAir.local>
2022-01-19 12:44:27 +0900

    fix: Change handleClick functino Issue #71
    to divide actions by type of button

--- client/src/components/FilterList/FilterList.jsx
+++ client/src/components/FilterList/FilterList.jsx
@@ -1,30 +1,36 @@
-import { useState } from 'react';
+import { string, arrayOf } from 'prop-types';
+import { useRef } from 'react';
 import { Selection } from 'components';
 import { Wrapper, Heading, List, Button } from './FilterList.styled';
 
-export const FilterList = ({ heading, options }) => {
-  const [isChecked, setChecked] = useState([]);
+export const FilterList = ({ type = 'checkbox', heading, options }) => {
+  const listRef = useRef(null);
   const isOver = options.length > 4;
   isOver && options.splice(4);
 
   const handleClick = (e) => {
     const selectionButton = e.target.children[1];
-    selectionButton.classList.toggle('active');
+    const isChecked = e.target.firstElementChild.checked;
+    const type = e.target.firstElementChild.type;
 
-    console.log(e.target);
+    if (type === 'checkbox')
+      selectionButton.classList.toggle('active', !isChecked);
+    else {
+      [...listRef.current.children].forEach((item) => {
+        const input = item.querySelector('input');
+        const label = item.querySelector('label');
+        label.classList.toggle('active', input.checked);
+      });
+    }
   };
 
   return (
     <Wrapper>
       <Heading>{heading}</Heading>
-      <List>
+      <List ref={listRef}>
         {options.map((item, key) => (
           <li key={key} tabIndex={'0'}>
-            <Selection
-              type="checkbox"
-              onClick={handleClick}
-              group={heading}
-              checked={isChecked[key]}>
+            <Selection type={type} onClick={handleClick} group={heading}>
               {item}
             </Selection>
           </li>
@@ -33,4 +39,10 @@ export const FilterList = ({ heading, options }) => {
       {isOver && <Button>See all</Button>}
     </Wrapper>
   );
+};
+
+FilterList.propTypes = {
+  type: string,
+  heading: string.isRequired,
+  options: arrayOf(string),
 };
