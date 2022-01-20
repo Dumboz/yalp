741029edc4dffb650cf56c079b602cea6f5dda0b
sonwonjae <sonwj091552@gmail.com>
2022-01-20 13:59:07 +0900

    feat: Test redux middleware

--- client/src/index.js
+++ client/src/index.js
@@ -3,29 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App, SearchPage, DetailPage } from 'pages';
 import { GlobalStyle } from 'styles/global.styled';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  useParams,
-  useSearchParams,
-  useLocation,
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { InitSVG } from 'components';
+import { Provider } from 'react-redux';
+import { store } from 'store/exam';
 
 ReactDOM.render(
   // <React.StrictMode>
   <>
     <GlobalStyle />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="search" element={<SearchPage />} />
-          <Route path=":id" element={<DetailPage />} />
-        </Route>
-      </Routes>
-      <InitSVG />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="search" element={<SearchPage />} />
+            <Route path=":id" element={<DetailPage />} />
+          </Route>
+        </Routes>
+        <InitSVG />
+      </BrowserRouter>
+    </Provider>
     {/* </React.StrictMode> */}
   </>,
   document.getElementById('root')
