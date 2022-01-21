cf5e72336277f65cee46aaa02f249419d7ec33fc
kheeyaa <kheeyaa@icloud.com>
2022-01-21 17:42:13 +0900

    feat: Update App component
    - Add Header and Pagenation component

--- client/src/pages/App/App.jsx
+++ client/src/pages/App/App.jsx
@@ -1,12 +1,19 @@
+import Header from 'components/Header/Header';
 import { Icon } from 'components/Icon/Icon';
 import { InitSVG } from 'components/InitSVG/InitSVG';
+import Pagenation from 'components/Pagenation/Pagenation';
 import SearchForm from 'components/SearchForm/SearchForm';
 import { Routes, Route, Link, Outlet } from 'react-router-dom';
 
 export function App() {
   return (
     <>
-      <SearchForm showLabel={true} hasShadow={true} locationWord="New York" />
+      {/* <SearchForm showLabel={true} hasShadow={true} locationWord="New York" /> */}
+      <Header
+        searchWord={`tacos, Cheap Dinner, Max's`}
+        locationWord="New York"
+      />
+      <Pagenation />
       <Outlet />
     </>
   );
