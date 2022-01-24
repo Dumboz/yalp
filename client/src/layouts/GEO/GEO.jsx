8cb1e20d6edeb76ac61cdb2f9744e59b1dde2a7f
sonwonjae <sonwj091552@gmail.com>
2022-01-24 13:20:23 +0900

    feat: Make it possible to search by country name - Issue #88
    - Make it possible to search by country name

--- client/src/layouts/GEO/GEO.jsx
+++ client/src/layouts/GEO/GEO.jsx
@@ -1,26 +1,47 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { Loader } from '@googlemaps/js-api-loader';
 import { Map, MapWrapper } from './GEO.styled';
+import { setAddress } from 'store/mapSlice';
+import { useSelector, useDispatch } from 'react-redux';
+
 export function GEO({ features: featuresList }) {
+  const ref = useRef();
+
+  const address = useSelector((state) => state.map.address);
+  const dispatch = useDispatch();
+  console.log(address);
+  setTimeout(() => {
+    dispatch(setAddress('New York'));
+  }, 1000);
+  console.log(address);
+
   useEffect(() => {
     const loader = new Loader({
       apiKey: 'AIzaSyAd3NJab1Ah3f59giU2nvfORfClh3_1xFo',
       version: 'weekly',
     });
-
-    let map;
-
-    const mapOptions = {
-      center: {
-        lat: 37.786882,
-        lng: -122.399972,
-      },
-      zoom: 14,
-    };
-
     loader.load().then((google) => {
-      map = new google.maps.Map(document.getElementById('map'), mapOptions);
+      const mapOptions = {
+        center: new google.maps.LatLng(37.786882, -122.399972),
+        zoom: 14,
+      };
 
+      let map = new google.maps.Map(ref.current, mapOptions);
+      let geocoder = new google.maps.Geocoder();
+
+      // Geocoder
+      geocoder.geocode({ address }, (results, status) => {
+        if (status === 'OK') {
+          console.log(results);
+          map.setCenter(results[0].geometry.location);
+        } else {
+          alert(
+            'Geocode was not successful for the following reason: ' + status
+          );
+        }
+      });
+
+      // Marker
       const features = featuresList.map(({ latitude, longitude }) => ({
         position: new google.maps.LatLng(latitude, longitude),
       }));
@@ -31,12 +52,14 @@ export function GEO({ features: featuresList }) {
           map: map,
         });
       });
+
+      // new google.maps.Geocoder().geocode({ latLng }, (results, status) => {});
     });
-  }, []);
+  });
 
   return (
     <MapWrapper>
-      <Map id="map" />
+      <Map id="map" ref={ref} />
     </MapWrapper>
   );
 }
