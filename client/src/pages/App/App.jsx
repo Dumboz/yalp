6a6f89e3386662996df97cc48c7f0dadcc7bc7f3
kheeyaa <kheeyaa@icloud.com>
2022-01-25 13:41:58 +0900

    style: Delete Test Code

--- client/src/pages/App/App.jsx
+++ client/src/pages/App/App.jsx
@@ -1,6 +1,5 @@
 import Header from 'components/Header/Header';
 import Pagenation from 'components/Pagenation/Pagenation';
-import PhotoModal from 'layouts/PhotoModal/PhotoModal';
 import { Routes, Route, Link, Outlet } from 'react-router-dom';
 
 export function App() {
@@ -11,34 +10,6 @@ export function App() {
         searchWord={`tacos, Cheap Dinner, Max's`}
         locationWord="New York"
       />
-      <PhotoModal
-        title="Dunkin'"
-        visible={true}
-        photos={[
-          'https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg',
-          'https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg',
-          'https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg',
-          // 'https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg',
-          // 'https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg',
-          // 'https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg',
-          // 'https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg',
-          // 'https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg',
-          // 'https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg',
-          // 'https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg',
-          // 'https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg',
-          // 'https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg',
-          // 'https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg',
-          // 'https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg',
-          // 'https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg',
-          // 'https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg',
-          // 'https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg',
-          // 'https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg',
-        ]}
-        select={
-          'https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg'
-        }
-      ></PhotoModal>
-
       <Outlet />
     </>
   );
