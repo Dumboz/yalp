3558b556259c47e918b68587089c5e1eb062b583
kheeyaa <kheeyaa@icloud.com>
2022-01-25 20:59:08 +0900

    fix: Solve Issue #107
    - Remove Redux state `businesses` dispatch logic in SearchPage
    - Use RTK Query state in SearchForm

--- client/src/pages/SearchPage/SearchPage.jsx
+++ client/src/pages/SearchPage/SearchPage.jsx
@@ -1,48 +1,21 @@
 import { useLocation } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useGetBusinessesQuery } from 'services/businesses';
 import { BusinessesList } from 'layouts';
 import { GEO } from 'layouts/GEO/GEO';
-import { SearchContainer } from './SearchPage.styled';
+import {
+  SearchContainer,
+  GEOWrapper,
+  FilterWrapper,
+} from './SearchPage.styled';
 import { FilterSection } from 'components';
 import { updateBusinesses } from 'store/businesses';
 import Pagenation from 'components/Pagenation/Pagenation';
 import { TitleWrapper } from 'components';
-import styled from 'styled-components';
-
-const GEOWrapper = styled.div`
-  width: 50%;
-  position: relative;
-`;
-
-const FilterWrapper = styled.div`
-  width: 30%;
-  position: relative;
-`;
 
 export function SearchPage() {
-  const dispatch = useDispatch();
   const { search } = useLocation();
   const { data, error, isLoading } = useGetBusinessesQuery(search);
-
-  let features = [];
-
-  if (data) {
-    features = data?.businesses.map(({ coordinates }) => coordinates);
-  }
-  if (isLoading) {
-    dispatch(updateBusinesses({ error, isLoading }));
-  } else {
-    dispatch(
-      updateBusinesses({
-        businesses: data.businesses,
-        total: data.total,
-        region: data.region,
-        error,
-        isLoading,
-      }),
-    );
-  }
 
   return (
     <SearchContainer>
@@ -61,7 +34,9 @@ export function SearchPage() {
       )}
       {data?.businesses && (
         <GEOWrapper>
-          <GEO features={features} />
+          <GEO
+            features={data?.businesses.map(({ coordinates }) => coordinates)}
+          />
         </GEOWrapper>
       )}
     </SearchContainer>
