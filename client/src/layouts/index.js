eb587cf6450b62ff234f1a2df663bc89567be37d
kheeyaa <kheeyaa@icloud.com>
2022-01-26 17:26:11 +0900

    config: Add DetailBanner in layouts/index.js

--- client/src/layouts/index.js
+++ client/src/layouts/index.js
@@ -1,3 +1,4 @@
 export * from './BusinessesList/BusinessesList';
 export * from './GEO/GEO';
 export * from './PhotoModal/PhotoModal';
+export { default as DetailBanner } from './DetailBanner/DetailBanner';
