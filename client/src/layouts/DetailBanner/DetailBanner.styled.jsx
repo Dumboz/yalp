1a8d5cf67cb90191fe79212f4ab8029da95401ca
kheeyaa <kheeyaa@icloud.com>
2022-01-26 17:25:32 +0900

    feat: Create DetailBanner component
    - Set PhotoModal
    - Set DetailContainer that includes CarouselWrapper and DetailWrapper

---
+++ client/src/layouts/DetailBanner/DetailBanner.styled.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const DetailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  position: relative;
+  background-color: black;
+  z-index: 0;
+  width: 100%;
+  height: 425px;
+  padding: 40px 64px;
+`;
+
+export const DetailWrapper = styled.div`
+  width: 100%;
+  max-width: 1000px;
+  margin: 0 auto;
+  z-index: 2;
+`;
+
+export const Title = styled.h1`
+  color: white;
+  font-weight: 900;
+  font-size: 40px;
+`;
+
+export const OpenTimeWrapper = styled.div`
+  margin-top: 8px;
+  display: flex;
+`;
+
+export const RatingWrapper = styled.div`
+  margin-bottom: 10px;
+`;
